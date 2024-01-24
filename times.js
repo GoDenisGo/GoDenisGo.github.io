f9b4b379c5f550164c491ee048d436ee765a4f23
Denis Krasniqi <officialdeniskras@gmail.com>
2024-01-24 22:56:02 +0000

    removed server from project and embedded favicons to html
    - deleted srvr.js
    - deleted times.js
    - added all favicons to index.html

--- times.js
+++
@@ -1,7 +0,0 @@
-// File "times.js" provides a set of functions for calculating different times.
-// (There is actually only one function as you can see, but the project structure feels much more readable by taking
-// advantage of modules to separate the function definitions and their usage from the actual web-server).
-export function timeOffset(offset) {
-  const current = new Date().getTime();
-  return new Date(current + offset * 3600 * 1000);
-}
